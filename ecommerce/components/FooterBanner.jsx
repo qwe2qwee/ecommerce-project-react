781b2bd68dce11073a357334540ad4d9088e7832
Hossin <94375287+qwe2qwee@users.noreply.github.com>
2023-11-14 22:40:04 +0300

    I finsh the banner section

---
+++ ecommerce/components/FooterBanner.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const FooterBanner = () => {
+  return <div>
+    footerBanner
+  </div>;
+};
+
+export default FooterBanner;
--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React from "react";
+import { HeroBanner, FooterBanner, Product } from "../components";
+import { client } from "../lib/client";
 
-const Home = () => {
+const Home = ({products,bannerdata}) => {
+  console.log(bannerdata[0].smallText
+    );
   return (
-    <div>
-      Home
-    </div>
+    <>
+      <HeroBanner HeroBanner={bannerdata.length && bannerdata[0] } />
+      <div className='products-heading'>
+        <h2>Beset Selling Products</h2>
+        <p>Speakers of many variations</p>
+      </div>
+      <div className='products-container'>
+        {products?.map((product) => <p key={product.id}>{product.name}</p> )}
+      </div>
+      <FooterBanner />
+    </>
   );
-}
+};
 
+export const getServerSideProps = async() => {
+  const query ='*[_type == "product"]'
+  const products = await client.fetch(query);
+
+  const Bannerquery ='*[_type == "banner"]'
+  const bannerdata = await client.fetch(Bannerquery);
+  return {props:{products ,bannerdata}}
+}
 export default Home;
