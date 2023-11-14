d772c3613e44487a1f4a1c70d67b07bdc0291438
Hossin <94375287+qwe2qwee@users.noreply.github.com>
2023-11-14 14:40:37 +0300

    start the work

---
+++ ecommerce/ecommercee/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'lhnr6v60',
+    dataset: 'production'
+  }
+})
--- ecommerce/pages/_app.js
+++ ecommerce/pages/_app.js
@@ -1,2 +1,13 @@
-import React from "react";
+import React from 'react';
+import "../styles/globals.css"
 
+
+function MyApp({ Component, pageProps }) {
+  return (
+
+        <Component {...pageProps} />
+
+  )
+}
+
+export default MyApp
